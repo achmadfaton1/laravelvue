0ad7d06e07d24537afa79a9d361485c77420e624
achmad fatoni <achmadfatoni7021@gmail.com>
2022-03-27 15:32:40 +0700

    index.vue

--- vue/src/views/transaction/Index.vue
+++ vue/src/views/transaction/Index.vue
@@ -1,5 +1,14 @@
 <template>
-    <h3>Index</h3>
+    <div class="container my-5">
+        <div class="row justify-content-center">
+            <div class="col-8">
+                <!-- <a href="/create">tes</a> -->
+                <router-link 
+                    :to="{ path: '/create'}" class="btn btn-primary btn-sm"
+                >Add</router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
